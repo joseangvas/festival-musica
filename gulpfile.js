9051c2b18e50fa5ff745fb7b1db77140eb2debb7
joseangvas <developer.javp@gmail.com>
2023-06-20 21:16:21 -0400

    Instalar y Configurar SASS y Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,20 @@
-function tarea( done ) {
-  console.log("Mi Primera Tarea");
+
+const { src, dest, watch } = require("gulp");
+const sass = require("gulp-sass") (require("sass"));
+
+function css( done ) {
+  src("src/scss/**/*.scss")
+    .pipe(sass())
+    .pipe(dest("build/css"))
+  
   done();
 }
 
-exports.tarea = tarea;
+function dev( done ) {
+  watch("src/scss/**/*.scss", css)
+
+  done();
+}
+
+exports.css = css;
+exports.dev = dev;
