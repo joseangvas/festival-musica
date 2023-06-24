22f7405ac2f2cd18e920bf457880c70ae2ac94d9
joseangvas <developer.javp@gmail.com>
2023-06-24 17:59:27 -0400

    Aligerar Imágenes

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
-
-const { src, dest, watch } = require("gulp");
+// Gulp
+const { src, dest, watch, parallel } = require("gulp");
 
 // CSS
-
 const sass = require("gulp-sass") (require("sass"));
 const plumber = require("gulp-plumber");
 
 // Imágenes
+const cache = require("gulp-cache");
+const imagemin = require("gulp-imagemin");
+const webp = require("gulp-webp");
 
 function css( done ) {
   src("src/scss/**/*.scss")
@@ -16,10 +18,34 @@ function css( done ) {
   done();
 }
 
+function imagenes( done ) {
+  const opciones = {
+    optimizationLevel: 3
+  }
+
+  src('src/img/**/*.{jpg, png, bmp}')
+    .pipe(cache( imagemin(opciones)))
+    .pipe(dest('build/img'))
+  done();
+}
+
+function versionWebp( done ) {
+  const opciones = {
+    quality: 50
+  };
+
+  src('src/img/**/*.{jpg, png, bmp}')
+    .pipe( webp(opciones) )
+    .pipe( dest('build/img') )
+  done();
+}
+
 function dev( done ) {
   watch("src/scss/**/*.scss", css)
   done();
 }
 
 exports.css = css;
-exports.dev = dev;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.dev = parallel(imagenes, versionWebp, dev);  // Ejecuta las tareas al mismo tiempo
